24dc7731d6299a2f0abb2a3b376470540dbfa496
Joe Winter <joe@joeventures.com>
2018-01-28 16:41:40 -0500

    hal asks about the weather in a mentioned classmate's city

--- js/index.js
+++ js/index.js
@@ -5,6 +5,30 @@ var chatInput = document.getElementById('chatInput');
 var dave = document.getElementById('dave');
 var hal = document.getElementById('hal');
 
+var classMates = {
+  "Joe": "Atlanta",
+  "Zach": "Stamford",
+  "David": "D.C.",
+  "Derek": "Boston",
+  "Christie": "D.C",
+  "Liana": "Baltimore",
+  "Mark": "D.C.",
+  "Carolyn": "Bay Area",
+  "Rebecca": "New York City",
+  "Bryan": "Los Angeles",
+  "Morris": "Los Angeles",
+  "Bailey": "New York City",
+  "Elaine": "D.C.",
+  "Kenneth": "D.C.",
+  "Courtney": "Oz",
+  "Andreina": "Siam",
+  "Ellen": "the Sleep-Tite Pajama Factory",
+  "Emily": "Mudfog",
+  "Whitney": "the Opera Populaire",
+  "Richard": "Paris",
+  "Sonyl": "New York City"
+};
+
 // add an event listener to the form to submit Dave's message
 chatForm.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -15,12 +39,29 @@ chatForm.addEventListener("submit", function(e) {
 
 function daveSpeak(daveSays) {
   dave.innerHTML = '<div class="chat-line">' + daveSays + '</div>' + dave.innerHTML;
+  halRespond(daveSays);
 }
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 // Dave's inputs
 function halRespond(daveSays) {
+  response = "I'm sorry, Dave. I didn't understand that.";
+  weatherAsk = includesClassmate(daveSays);
+  if(weatherAsk) {
+    city = classMates[weatherAsk];
+    response = "Ask " + weatherAsk + " how the weather is in " + city + "!";
+  }
+  halSpeak(response);
+}
 
+function includesClassmate(daveSays) {
+  var result = false;
+  Object.keys(classMates).forEach(function(classMate) {
+    if(daveSays.includes(classMate)) {
+      result = classMate;
+    }
+  });
+  return result;
 }
 
 function halSpeak(halSays) {
