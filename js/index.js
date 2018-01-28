a21e12ec1f4f559318092e5f0c02533ef33421c0
Joe Winter <joe@joeventures.com>
2018-01-28 15:17:16 -0500

    basic event listener

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ var dave = document.getElementById('dave');
 var hal = document.getElementById('hal');
 
 // add an event listener to the form to submit Dave's message
+chatForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  daveSays = chatInput.value;
+  chatInput.value = "";
+});
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 // Dave's inputs
