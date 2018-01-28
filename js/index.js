3ccf4f36b549e12f28c978d2fbaf20c6b707dee0
Joe Winter <joe@joeventures.com>
2018-01-28 14:45:49 -0500

    Set up pointers

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// Set up some pointers
+var chatForm = document.getElementById('chatForm');
+var chatBot = document.getElementById('chatbot');
+var chatInput = document.getElementById('chatInput');
+var dave = document.getElementById('dave');
+var hal = document.getElementById('hal');
+
 // add an event listener to the form to submit Dave's message
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
